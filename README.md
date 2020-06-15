80a14463e5892ce1932f6356244559845aeefff8
oleksiv <hi.oleksiv@gmail.com>
2020-06-15 20:16:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-jtqr4d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-jtqr4d)
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, VERSION, OnInit } from '@angular/core';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit  {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      query: this.fb.control(''),
+    });
+  }
+}
